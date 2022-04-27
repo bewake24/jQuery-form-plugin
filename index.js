f1355256ab2e282b59f67b6520979dbc8323363e
Vivek Kumar <Kumarvivek79510@gmail.com>
2022-04-27 14:47:08 +0530

    Initial Upload

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
-/* Opening Form */
-document.querySelector(".mess").addEventListener("click", openForm);
 
-function openForm() {
-  document.querySelector(".hideform").style.display = "block"; //querySelector() makes changes in the object while getElement.... only gets the elements.
-  //alert("Form opened");
-}
+/* To open form */
+$(document).ready(function(){
+  $(".mess").click(function(){
+    $(".hideform").show();
+  });
+});
 
-/* Closing Form */
-document.querySelector(".close").addEventListener("click", closeForm);
-
-function closeForm() {
-  document.querySelector(".hideform").style.display = "none";
-  //alert("Form closed");
-}
-
-/* jQuery */
-// These javascript files are to be converted into jQuery.
+/* To close form */
+$(document).ready(function(){
+  $(".close").click(function(){
+    $(".hideform").hide();
+  });
+});
